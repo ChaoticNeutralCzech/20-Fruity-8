60d4a584333712aa7b5c424c4097517b8cc1af50
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-22 17:47:08 +0800

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -48,12 +48,14 @@ GameManager.prototype.setup = function () {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
+    this.moves       = previousState.moves;
     this.over        = previousState.over;
     this.won         = previousState.won;
     this.keepPlaying = previousState.keepPlaying;
   } else {
     this.grid        = new Grid(this.size);
     this.score       = 0;
+    this.moves       = 0;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
@@ -99,6 +101,7 @@ GameManager.prototype.actuate = function () {
   this.actuator.actuate(this.grid, {
     score:      this.score,
     over:       this.over,
+    moves:	this.moves++,
     won:        this.won,
     bestScore:  this.storageManager.getBestScore(),
     terminated: this.isGameTerminated()
@@ -111,6 +114,7 @@ GameManager.prototype.serialize = function () {
   return {
     grid:        this.grid.serialize(),
     score:       this.score,
+    moves:       this.moves,
     over:        this.over,
     won:         this.won,
     keepPlaying: this.keepPlaying
