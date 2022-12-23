cd0b257e205ef81b0a9652a5a917720eddc6fc1b
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 09:35:00 +0800

    Removed it there

--- js/application.js
+++ js/application.js
@@ -1,7 +1,3 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js");
-}
