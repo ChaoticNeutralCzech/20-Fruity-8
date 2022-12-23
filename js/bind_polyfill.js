fdcc66ff6e9f41aad6d9b9dc5bac28bf06798162
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 10:16:01 +0800

    Delete js directory

--- js/bind_polyfill.js
+++
@@ -1,9 +0,0 @@
-Function.prototype.bind = Function.prototype.bind || function (target) {
-  var self = this;
-  return function (args) {
-    if (!(args instanceof Array)) {
-      args = [args];
-    }
-    return self.apply(target, args);
-  };
-};
