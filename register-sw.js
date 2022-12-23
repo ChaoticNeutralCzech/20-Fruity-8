160d54978ca47c7036a0a0a63fb034608e91382f
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 09:58:22 +0800

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller) {
     console.log("Active service worker found");
     } else {
         navigator.serviceWorker
-        .register("sw.js"), {
+        .register("https://thedoggybrad.github.io/2048ontheweb/sw.js"), {
         scope: "./"
         })
         .then(function (reg) {
