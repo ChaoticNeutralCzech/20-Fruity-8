670a61ec08f2735892e876d802b77bf65360788d
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 10:04:10 +0800

    Fixes an issue that brokes PWA

--- register-sw.js
+++ register-sw.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller) {
     console.log("Active service worker found");
     } else {
         navigator.serviceWorker
-        .register("https://thedoggybrad.github.io/2048ontheweb/sw.js"), {
+        .register("sw.js"), {
         scope: "./"
         })
         .then(function (reg) {
