bb02a0ade76e605e33639d563729a720f036724f
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-26 17:44:47 +0800

    Update game2.js

--- js/game2.js
+++ js/game2.js
@@ -379,3 +379,9 @@ GameManager.prototype.positionsEqual = function (first, second) {
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+window.addEventListener("contextmenu", e => e.preventDefault());
+
+function myalert() {
+		alert("Swipe using your touchscreen mobile device and use the keyboard arrow keys on your PC/Laptop. Tiles with the same number merge into one when they touch. Add them up until you reach 2048!");
+	}
