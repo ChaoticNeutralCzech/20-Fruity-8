773b853216103a1fbce218609a9d00b88ab31751
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-26 18:02:47 +0800

    No need for this directory

--- js/howtoplay.js
+++
@@ -1,3 +0,0 @@
-function myalert() {
-		alert("Swipe using your touchscreen mobile device and use the keyboard arrow keys on your PC/Laptop. Tiles with the same number merge into one when they touch. Add them up until you reach 2048!");
-	}
