b473a010b42a200a65cb1d0aff5c6cc4096ce9b4
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-01-01 13:51:10 +0800

    Update game.js

--- game.js
+++ game.js
@@ -922,7 +922,36 @@ window.requestAnimationFrame(function () {
 
 window.addEventListener("contextmenu", e => e.preventDefault());
 
+
+
 function myalert() {
 		alert("Swipe using your touchscreen mobile device and use the keyboard arrow keys on your PC/Laptop. Tiles with the same number merge into one when they touch. Add them up until you reach 2048!");
 	}
 
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
